4705c6d4696558abd4c3af4318337d3bb05358e0
Asdfobuy <yubo.tian01@gmail.com>
2023-09-24 11:42:11 -0700

    hibakeresés

--- main.cpp
+++ main.cpp
@@ -4,18 +4,18 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS];                //nem egyeznek a változó nevek
+    std::cout << '1-100 ertekek duplazasa'      //nem egy aposztróf, hanem duplának kell lennie, illetve pontosvessző hiányzik
+    for (int i = 0;)                            //hiányzik az "i = 0", után az "i < N_ELEMENTS; i++"
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++)                    //hiányzik az i = N_ELEMENTS
     {
         std::cout << "Ertek:"
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
+    int atlag;                                  // int atlag = 0;
     for (int i = 0; i < N_ELEMENTS, i++)
     {
         atlag += b[i]
